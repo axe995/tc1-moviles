67475b9a49c8ea6f393a59d378fac4dce19d555a
axe995 <axe995@gmail.com>
2014-03-19 23:17:55 -0600

    Test factoriales

---
+++ JniApp.java
@@ -0,0 +1,11 @@
+public class JniApp
+{
+	public native int factorial(int number);
+	public static void main(String[] args)
+	{
+		JniApp ja = new JniApp();
+		
+		int f = ja.factorial(500);
+		System.out.println("Factorial of 5 is: "+f);
+	}
+}
